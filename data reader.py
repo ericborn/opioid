6851732f7f1f4ba65bdfd79a05213926eec1beb2
Eric Born <eric.born85@gmail.com>
2019-11-15 13:08:44 -0800

    SQL to create table

--- data reader.py
+++ data reader.py
@@ -14,20 +14,22 @@ path = r'F:\opioid data'
 file = 'arcos_all_washpost'
 full_path = os.path.join(path, file + '.tsv')
 
-# read header row
-pd.read_csv(full_path,nrows=1) 
-    
-   
-with open(full_path) as tsvfile:
-  reader = csv.DictReader(tsvfile, dialect='excel-tab')
-  for row in reader:
-      print(row)
-        
-# read csv file into dataframe
-try:
-    lol_df = pd.read_csv(ticker_file)
-    print('opened file for ticker: ', data,'\n')
+# read 10 rows
+arcos_df = pd.read_csv(full_path,nrows=500, sep='\t') 
 
-except Exception as e:
-    print(e)
-    exit('failed to read LoL data from: '+ str(data)+'.csv')
+# output 
+arcos_df.to_csv(path+'\\test.csv')
+   
+#with open(full_path) as tsvfile:
+#  reader = csv.DictReader(tsvfile, dialect='excel-tab')
+#  for row in reader:
+#      print(row)
+#        
+## read csv file into dataframe
+#try:
+#    lol_df = pd.read_csv(ticker_file)
+#    print('opened file for ticker: ', data,'\n')
+#
+#except Exception as e:
+#    print(e)
+#    exit('failed to read LoL data from: '+ str(data)+'.csv')
