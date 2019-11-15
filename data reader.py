e1958b0e8014ce84a3ce7f37c936af30d2731e6f
Eric Born <eric.born85@gmail.com>
2019-11-15 12:09:59 -0800

    Update data reader.py

--- data reader.py
+++ data reader.py
@@ -4,14 +4,30 @@ Eric Born
 
 File to read the arcos tsv files and move the data into postgre db
 """
-
+import os
 import csv
+import pandas as pd
+from sys import exit
+
 
 path = r'F:\opioid data'
+file = 'arcos_all_washpost'
+full_path = os.path.join(path, file + '.tsv')
 
-file = 'arcos_all_washpost.tsv'
-
-with open('myfile.tsv') as tsvfile:
+# read header row
+pd.read_csv(full_path,nrows=1) 
+    
+   
+with open(full_path) as tsvfile:
   reader = csv.DictReader(tsvfile, dialect='excel-tab')
   for row in reader:
-    print(row)
+      print(row)
+        
+# read csv file into dataframe
+try:
+    lol_df = pd.read_csv(ticker_file)
+    print('opened file for ticker: ', data,'\n')
+
+except Exception as e:
+    print(e)
+    exit('failed to read LoL data from: '+ str(data)+'.csv')
