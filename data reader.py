3c3e7fb124d2238f6aa3a54510226cc625d55970
Eric Born <eric.born85@gmail.com>
2019-11-15 15:30:51 -0800

    Update data reader.py

--- data reader.py
+++ data reader.py
@@ -9,16 +9,31 @@ import csv
 import pandas as pd
 from sys import exit
 
-
 path = r'F:\opioid data'
 file = 'arcos_all_washpost'
 full_path = os.path.join(path, file + '.tsv')
 
-# read 10 rows
-arcos_df = pd.read_csv(full_path,nrows=500, sep='\t') 
+# read rows
+arcos_df = pd.read_csv(full_path,nrows=500, sep='\t')
+
+# view transaction data column
+#arcos_df.iloc[:,30]
+
+# view row 89, all columns
+#arcos_df.iloc[89,:]
+
+# view by column name
+#arcos_df['QUANTITY']
+
+# converts int format to month/day/year
+arcos_df['TRANSACTION_DATE'] = pd.to_datetime(arcos_df['TRANSACTION_DATE'], format='%m%d%Y')
+
+# convert QUANTITY from float to int
+arcos_df['QUANTITY'] = arcos_df['QUANTITY'].astype('int64')
 
 # output 
-arcos_df.to_csv(path+'\\test.csv')
+arcos_df.to_csv(path+'\\test.csv', index = False)
+
    
 #with open(full_path) as tsvfile:
 #  reader = csv.DictReader(tsvfile, dialect='excel-tab')
