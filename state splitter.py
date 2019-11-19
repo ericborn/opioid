1d9d61807550aae2989072dba92136c6bcc220d5
Eric Born <eric.born85@gmail.com>
2019-11-19 12:29:57 -0800

    Update state splitter.py
    Fixed code that generates a dynamic SQL query for the state name and initials then inserts data from opioids table based upon the buyer_state column

--- state splitter.py
+++ state splitter.py
@@ -5,105 +5,94 @@ Created on Tue Nov 19 10:08:24 2019
 @author: Eric
 """
 #from psycopg2 import connect, DatabaseError
-#from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
 import psycopg2
 from sys import exit
-from sqlalchemy import create_engine, MetaData, insert, Table, Column, String,\
-                       Integer, Float, Boolean, VARCHAR, SmallInteger
-
-states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado',
-          'Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho',
-          'Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana',
-          'Maine','Maryland','Massachusetts','Michigan','Minnesota',
-          'Mississippi','Missouri','Montana','Nebraska','Nevada',
-          'New_Hampshire','New_Jersey','New_Mexico','New_York',
-          'North_Carolina','North_Dakota','Ohio','Oklahoma','Oregon',
-          'Pennsylvania','Rhode_Island','South_Carolina','South_Dakota',
-          'Tennessee','Texas','Utah','Vermont','Virginia','Washington',
-          'West_Virginia','Wisconsin','Wyoming']
-
-initials = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL',
-           'IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT',
-           'NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI',
-           'SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
-
-for st in states: 
-    i = 0
-    
-    INSERT INTO st
-    SELECT *
-    FROM opioids
-    WHERE state = initials[i]
-    
-    i += 1
+from psycopg2 import sql
+#from psycopg2.extensions import quote_ident
+from sqlalchemy import create_engine
 
 #####
 # 3 state test
 #####
     
-states_test = ['Alabama','Alaska','Arizona']
+states_test = ['alabama','alaska','arizona']
 initials_test = ['AL','AK','AZ']
-
-i = 0
 
 ###
 # Start SQL
 ###
 
 # Creates a connection string
+# SQL Alchemy
 engine = create_engine('postgresql+psycopg2://python:password@localhost/arcos')
 
 # raw connection
 conn = engine.raw_connection()
 
+
 # Opens a cursor to write the data
 cur = conn.cursor()
 
-for st in states_test: 
-    print(states_test[i], initials_test[i])
-    i += 1
+#query = sql.SQL("INSERT INTO {0} SELECT * FROM opioids WHERE buyer_state = {1}").format(
+#            sql.Identifier(states_test[0]), sql.Literal(initials_test[0]))
+
+#print(query.as_string(cur))
+
+#cur.execute(query)
+
+i = 0
+
+for state in states_test: 
+    table_name = states_test[i]
+    init_name = initials_test[i]
     
-cur.executemany(
-    '''
-    INSERT INTO ?
-    SELECT *
-    FROM opioids
-    WHERE state = ?
-    ''', (states_test, initials_test)
-)
+    #print(i)
+    # stores the current state and state initial into the query
+    # query inserts into state name table with * selected from opioids table
+    # where buyer_state is equal to the states initials
+    # formats the table name to an identifier and 
+    # the initials to a string literal
+    query = sql.SQL("INSERT INTO {0} SELECT * FROM opioids WHERE buyer_state = {1}").format(
+            sql.Identifier(table_name), sql.Literal(init_name))
+    
+    #print(query.as_string(cur))
+    
+    # executes the query
+    cur.execute(query)
+    i += 1
+
 cur.close()
 conn.commit()
 conn.close()
 
-INSERT INTO st
-SELECT *
-FROM opioids
-WHERE state = initials_test[i]
-    
-    
-    
-try:
-    #Connects and creates the desired database
-    conn = connect('dbname=arcos user=python password=password')
-
-    #conn.setAutoCommit(true)
-    cur = conn.cursor()
-    #cur.execute('SET AUTOCOMMIT = ON')
-    cur.executemany(
-        '''
-        INSERT INTO ?
-        SELECT *
-        FROM opioids
-        WHERE state = ?
-        ''', (states_test, initials_test)
-    )
-    cur.close()
-    conn.commit()
-    conn.close()
-except (Exception, DatabaseError) as dbError:
-    print(dbError)
-
-
 ####
 # End SQL
 ####
+
+
+#states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado',
+#          'Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho',
+#          'Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana',
+#          'Maine','Maryland','Massachusetts','Michigan','Minnesota',
+#          'Mississippi','Missouri','Montana','Nebraska','Nevada',
+#          'New_Hampshire','New_Jersey','New_Mexico','New_York',
+#          'North_Carolina','North_Dakota','Ohio','Oklahoma','Oregon',
+#          'Pennsylvania','Rhode_Island','South_Carolina','South_Dakota',
+#          'Tennessee','Texas','Utah','Vermont','Virginia','Washington',
+#          'West_Virginia','Wisconsin','Wyoming']
+#
+#initials = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL',
+#           'IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT',
+#           'NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI',
+#           'SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+#
+#for st in states: 
+#    i = 0
+#    
+#    INSERT INTO st
+#    SELECT *
+#    FROM opioids
+#    WHERE state = initials[i]
+#    
+#    i += 1
