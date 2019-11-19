6bac71d26026aa5c6d0802a5a278efbb5f22cbdc
Eric Born <eric.born85@gmail.com>
2019-11-18 20:46:31 -0800

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,7 +1,249 @@
---truncate opioids;
---select count(*) from opioids
-
-INSERT INTO alabama
+INSERT INTO Alabama 
 SELECT * 
 FROM opioids
-WHERE buyer_state = ''
+WHERE buyer_state = 'AL';
+
+INSERT INTO Alaska 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'AK';
+
+INSERT INTO Arizona 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'AZ';
+
+INSERT INTO Arkansas 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'AR';
+
+INSERT INTO California 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'CA';
+
+INSERT INTO Colorado 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'CO';
+
+INSERT INTO Connecticut 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'CT';
+
+INSERT INTO Delaware 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'DE';
+
+INSERT INTO Florida 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'FL';
+
+INSERT INTO Georgia 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'GA';
+
+INSERT INTO Hawaii 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'HI';
+
+INSERT INTO Idaho 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'ID';
+
+INSERT INTO Illinois 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'IL';
+
+INSERT INTO Indiana 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'IN';
+
+INSERT INTO Iowa
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'IA';
+
+INSERT INTO Kansas 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'KS';
+
+INSERT INTO Kentucky 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'KY';
+
+INSERT INTO Louisiana 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'LA';
+
+INSERT INTO Maine 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'ME';
+
+INSERT INTO Maryland 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'MD';
+
+INSERT INTO Massachusetts 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'MA';
+
+INSERT INTO Michigan 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'MI';
+
+INSERT INTO Minnesota 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'MN';
+
+INSERT INTO Mississippi 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'MS';
+
+INSERT INTO Missouri 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'MO';
+
+INSERT INTO Montana 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'MT';
+
+INSERT INTO Nebraska 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'NE';
+
+INSERT INTO Nevada 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'NV';
+
+INSERT INTO New_Hampshire
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'NH';
+
+INSERT INTO New_jersey
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'NJ';
+
+INSERT INTO New_Mexico
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'NM';
+
+INSERT INTO New_York
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'NY';
+
+INSERT INTO North_Carolina
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'NC';
+
+INSERT INTO North_Dakota 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'ND';
+
+INSERT INTO Ohio 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'OH';
+
+INSERT INTO Oklahoma
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'OK';
+
+INSERT INTO Oregon
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'OR';
+
+INSERT INTO Pennsylvania 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'PA';
+
+INSERT INTO Rhode_Island 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'RI';
+
+INSERT INTO South_carolina
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'SC';
+
+INSERT INTO South_Dakota
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'SD';
+
+INSERT INTO Tennessee 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'TN';
+
+INSERT INTO Texas 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'TX';
+
+INSERT INTO Utah
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'UT';
+
+INSERT INTO Vermont 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'VT';
+
+INSERT INTO Virginia
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'VA';
+
+INSERT INTO Washington
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'WA';
+
+INSERT INTO West_Virginia
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'WV';
+
+INSERT INTO Wisconsin 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'WI';
+
+INSERT INTO Wyoming 
+SELECT * 
+FROM opioids
+WHERE buyer_state = 'WY';
