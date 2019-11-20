ec4b36edb8035df0f81bce68e83d2bd95dfd3964
Eric Born <eric.born85@gmail.com>
2019-11-19 17:32:16 -0800

    Update Table create.sql

--- Table create.sql
+++ Table create.sql
@@ -1,6 +1,17 @@
---DROP TABLE IF EXISTS opioids;
+--DROP TABLE IF EXISTS
+--Alabama,Alaska,Arizona,Arkansas,California,Colorado,
+--Connecticut,Washington_dc,Delaware,Florida,Georgia,
+--Hawaii,Idaho,Illinois,Indiana,Iowa,Kansas,Kentucky,
+--Louisiana,Maine,Maryland,Massachusetts,Michigan,
+--Minnesota,Mississippi,Missouri,Montana,Nebraska,Nevada,
+--New_Hampshire,New_Jersey,New_Mexico,New_York,
+--North_Carolina,North_Dakota,Ohio,Oklahoma,Oregon,
+--Pennsylvania,Rhode_Island,South_Carolina,South_Dakota,
+--Tennessee,Texas,Utah,Vermont,Virginia,Washington,
+--West_Virginia,Wisconsin,Wyoming;
 
-CREATE TABLE washington_dc (
+
+CREATE TABLE opioids (
 --REPORTER_DEA_NO TEXT,
 --REPORTER_BUS_ACT TEXT,
 REPORTER_NAME TEXT,
