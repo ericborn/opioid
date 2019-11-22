149f27c2635a2e17b4cdffccb285dce11fbc5eba
Eric Born <eric.born85@gmail.com>
2019-11-22 14:18:16 -0800

    Update zip reader.py

--- zip reader.py
+++ zip reader.py
@@ -18,3 +18,8 @@ zip_df = pd.read_csv(full_path)
 # remove columns gameId, creationTime, seasonId and winner
 zip_df.drop(zip_df.columns[[1,2,3,4,5,6,8]], axis = 1, inplace = True)
 
+# TODO
+# pad 0's on zipcodes that start with 0 or 00
+
+# need to write a SQL statement to create a table for zip
+# columns: zip, population both int
