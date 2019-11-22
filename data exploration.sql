76753d84427fa7d42cc13bdbd952e3a6eb7f2a7f
Eric Born <eric.born85@gmail.com>
2019-11-22 14:14:41 -0800

    Update data exploration.sql

--- data exploration.sql
+++ data exploration.sql
@@ -1,20 +1,40 @@
-SELECT * FROM wyoming
-LIMIT 1
+/*
+!!!TODO!!!
+Fix blank dosage_units
+SELECT * 
+FROM wyoming
+WHERE dosage_unit IS NULL
+LIMIT 10
 
--- Counts by drug seller ordered descending
+
+*/
+SELECT * 
+FROM wyoming
+--WHERE dosage_unit IS NULL
+LIMIT 10
+
+-- Transaction counts by drug seller ordered descending
 SELECT DISTINCT reporter_address1, COUNT(*) AS count
 FROM wyoming
 GROUP BY reporter_address1
 ORDER BY count DESC
 
--- Counts by drug buyer ordered descending
+-- Transaction counts by drug buyer ordered descending
 SELECT DISTINCT buyer_address1, COUNT(*) AS count
 FROM wyoming
 GROUP BY buyer_address1
 ORDER BY count DESC
 
--- Counts by buyer 
+-- Transaction counts grouped by buyer and seller
 SELECT DISTINCT buyer_address1, reporter_address1, COUNT(*) AS count
 FROM wyoming
 GROUP BY buyer_address1, reporter_address1
-ORDER BY buyer_address1
+ORDER BY buyer_address1, count DESC
+
+-- Dosage sum by drug seller grouped by reporter_name and year
+SELECT DISTINCT reporter_name, EXTRACT(YEAR FROM transaction_date) AS year,
+				SUM(dosage_unit) AS Dosage
+FROM wyoming
+WHERE dosage_unit IS NOT NULL
+GROUP BY reporter_name, year
+ORDER BY reporter_name, year, dosage DESC
