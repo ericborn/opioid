6675841bf188a3cf77447fcb1bdbc7000fcda7bb
Eric Born <eric.born85@gmail.com>
2019-11-22 14:15:26 -0800

    Create zip reader.py
    Add populations by zip for 2010

---
+++ zip reader.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 22 14:03:57 2019
+
+@author: Eric
+"""
+import os
+import pandas as pd
+
+# setup file location
+path = r'C:\Code projects\opioids'
+file = 'zips'
+full_path = os.path.join(path, file + '.csv')
+
+# read rows set number of rows
+zip_df = pd.read_csv(full_path)
+
+# remove columns gameId, creationTime, seasonId and winner
+zip_df.drop(zip_df.columns[[1,2,3,4,5,6,8]], axis = 1, inplace = True)
+
