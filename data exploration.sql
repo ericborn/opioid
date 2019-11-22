207c7e4f579eb942d0cda045c95e15b4bd97f9e2
Eric Born <eric.born85@gmail.com>
2019-11-22 15:33:29 -0800

    Update data exploration.sql
    Added more queries

--- data exploration.sql
+++ data exploration.sql
@@ -6,7 +6,6 @@ FROM wyoming
 WHERE dosage_unit IS NULL
 LIMIT 10
 
-
 */
 SELECT * 
 FROM wyoming
@@ -33,8 +32,28 @@ ORDER BY buyer_address1, count DESC
 
 -- Dosage sum by drug seller grouped by reporter_name and year
 SELECT DISTINCT reporter_name, EXTRACT(YEAR FROM transaction_date) AS year,
-				SUM(dosage_unit) AS Dosage
+				ROUND(SUM(dosage_unit)) AS Dosage
 FROM wyoming
 WHERE dosage_unit IS NOT NULL
 GROUP BY reporter_name, year
 ORDER BY reporter_name, year, dosage DESC
+
+-- Dosage sum by buyer_city grouped by buyer_city and year
+SELECT DISTINCT buyer_city, EXTRACT(YEAR FROM transaction_date) AS year,
+				ROUND(SUM(dosage_unit)) AS Dosage
+FROM wyoming
+WHERE dosage_unit IS NOT NULL
+GROUP BY buyer_city, year
+ORDER BY buyer_city, year, dosage DESC
+
+-- TODO
+-- Numbers need to be evaluated for accuracy
+-- Join zip on state table to find population and total dosage by zip
+SELECT DISTINCT buyer_zip, EXTRACT(YEAR FROM transaction_date) AS year, z.population,
+				ROUND(SUM(dosage_unit)) AS Dosage, 
+				(ROUND(SUM(dosage_unit)) / z.population) AS ratio
+FROM wyoming w
+INNER JOIN zip z ON (z.zip = w.buyer_zip)
+WHERE dosage_unit IS NOT NULL AND EXTRACT(YEAR FROM transaction_date) = 2010
+GROUP BY buyer_zip, year, population
+ORDER BY dosage DESC
